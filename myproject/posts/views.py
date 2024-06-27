ebf9efb69ab9ab10130ee3474e740456cf29401f
Tirth <tirth.rathod@naapbooks.com>
2024-06-27 15:09:25 +0530

    add new post

--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from django.contrib.auth.decorators import login_required
+from . import forms
 
 # Create your views here.
 
@@ -17,4 +18,13 @@ def post_page(request, slug):
 
 @login_required(login_url="/users/login/")
 def post_new(request):
-    return render(request, "posts/post_new.html")
+    if request.method == "POST":
+        form = forms.CreatePost(request.POST, request.FILES)
+        if form.is_valid():
+            newpost = form.save(commit=False)
+            newpost.author = request.user
+            newpost.save()
+            return redirect("posts:list")
+    else:
+        form = forms.CreatePost()
+    return render(request, "posts/post_new.html", {"form": form})
