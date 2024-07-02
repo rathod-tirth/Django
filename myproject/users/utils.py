049e0365a42fa44707005c1c91d96179fcca28ef
Tirth <tirth.rathod@naapbooks.com>
2024-07-02 19:35:38 +0530

    email with attachment

--- myproject/users/utils.py
+++ myproject/users/utils.py
@@ -1,4 +1,4 @@
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMessage
 from django.conf import settings
 
 
@@ -6,10 +6,19 @@ def send_mails(val):
     subject = val.subject
     message = val.message
     from_email = settings.EMAIL_HOST_USER
-    to_email = [val.to_email]
-    # attachments=val.attachments
-    status = send_mail(subject, message, from_email, to_email)
-    if status:
-        return True
-    else:
-        return False
+    to_email = val.to_email
+    attachment = val.attachment
+    mail = send_mail(subject, message, from_email, [to_email])
+    # if status:
+    #     return True
+    # else:
+    #     return False
+
+
+def SendEmailMessage(subject, message, to_email, attachment):
+    mail = EmailMessage(
+        subject, body=message, from_email=settings.EMAIL_HOST_USER, to=[to_email]
+    )
+    if attachment:
+        mail.attach(attachment.name, attachment.read(), attachment.content_type)
+    mail.send()
