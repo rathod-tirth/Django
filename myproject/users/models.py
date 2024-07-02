634be5a60ecf7ed988b674282d648fa8dfe0d24d
Tirth <tirth.rathod@naapbooks.com>
2024-07-02 17:48:56 +0530

    add email model

--- myproject/users/models.py
+++ myproject/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class SendEmail(models.Model):
+    subject = models.CharField(max_length=50)
+    message = models.TextField()
+    from_email = models.EmailField(max_length=254)
+    to_email = models.EmailField(max_length=254)
+    status = models.BooleanField()
+    attachment = models.FileField(upload_to=None, max_length=100, blank=True)
+
+    def __str__(self):
+        return f"{self.to_email} | {self.subject}"
