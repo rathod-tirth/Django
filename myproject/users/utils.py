764188146f4d80fa7ceeea7913742bb7c553b5be
Tirth <tirth.rathod@naapbooks.com>
2024-07-03 16:52:16 +0530

    add send email with template function

--- myproject/users/utils.py
+++ myproject/users/utils.py
@@ -1,24 +1,42 @@
-from django.core.mail import send_mail, EmailMessage
+from django.core.mail import send_mail, EmailMessage, EmailMultiAlternatives
 from django.conf import settings
+from django.template.loader import render_to_string
 
 
+# send mail without attachment
 def send_mails(val):
-    subject = val.subject
-    message = val.message
-    from_email = settings.EMAIL_HOST_USER
-    to_email = val.to_email
-    attachment = val.attachment
-    mail = send_mail(subject, message, from_email, [to_email])
-    # if status:
-    #     return True
-    # else:
-    #     return False
+    status = send_mail(
+        subject=val.subject,
+        message=val.message,
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[val.to_email],
+    )
+    if status:
+        return True
+    else:
+        return False
 
 
+# send mail with attachment
 def SendEmailMessage(subject, message, to_email, attachment):
     mail = EmailMessage(
         subject, body=message, from_email=settings.EMAIL_HOST_USER, to=[to_email]
     )
+    if attachment:
+        mail.attach(attachment.name, attachment.read(), attachment.content_type)
+    mail.send()
+
+
+# sending email with html template
+def SendEmailWithTemplate(subject, message, username, to_email, template, attachment):
+    mail = EmailMultiAlternatives(
+        subject, body=message, from_email=settings.EMAIL_HOST_USER, to=[to_email]
+    )
+
+    # rendering template
+    html_template = render_to_string(template, {"username": username})
+    mail.attach_alternative(html_template, "text/html")
+
     if attachment:
         mail.attach(attachment.name, attachment.read(), attachment.content_type)
     mail.send()
